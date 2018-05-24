bddb25664f7c7ffbbd5dfbc94d1ca87babb23984
matheusm94 <mmatos.moreira@gmail.com>
2018-05-23 23:32:53 -0300

    Update banco.sql

--- banco.sql
+++ banco.sql
@@ -3,7 +3,7 @@ CREATE TABLE passageiro
 nome VARCHAR(40)NOT NULL,
 cpf VARCHAR (20) NOT NULL,
 nascimento VARCHAR(20)NOT NULL,
-sexo VARCHAR(20)NOT NULL
+sexo VARCHAR(10)NOT NULL
 
 );
 
@@ -11,16 +11,25 @@ sexo VARCHAR(20)NOT NULL
 ALTER TABLE passageiro
 ADD PRIMARY KEY (nome)
 
+CREATE TABLE motorista
+(
+nome VARCHAR(40)NOT NULL,
+cpf VARCHAR (10) NOT NULL,
+nascimento VARCHAR(20)NOT NULL,
+sexo VARCHAR(10)NOT NULL,
+modeloCarro(10)NOT NULL,  
+statusMotorista(10) NOT NULL
+);
+
 ALTER TABLE motorista
-ADD PRIMARY KEY (nome)
+ADD PRIMARY KEY (cpf)
 
 CREATE TABLE corrida
 (
-motorista VARCHAR(20),
-passageiro VARCHAR(20),
+cpfMotorista VARCHAR(10),
+cpfPassageiro VARCHAR(10),
 valor FLOAT (20)NOT NULL,
-FOREIGN KEY (motorista) REFERENCES motorista(nome)
+CONSTRAINT CPK_corrida PRIMARY KEY (cpfMotorista, cpfPassageiro),
+CONSTRAINT FK_cpf_passageiro FOREIGN KEY (cpfMotorista) REFERENCES motorista (cpf),
+CONSTRAINT FK_cpf_motorista FOREIGN KEY (cpfPassageiro) REFERENCES passageiro (cpf)
 );
-
-ALTER TABLE corrida
-ADD FOREIGN KEY (passageiro) REFERENCES passageiro(nome); 
